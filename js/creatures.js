136a1f80401b26020ab400e459dca38117b67bea
Vale310 <122951323+Vale310@users.noreply.github.com>
2026-05-29 15:48:47 -0700

    Add data attributes and implement creatures.js functions

--- js/creatures.js
+++ js/creatures.js
@@ -1,38 +1,35 @@
 
-        
-function showHome()
-{
-    /*
-        loop through all creatures
-        
-        if creature lives on land, background color green
-        if creature lives in water, blue
-        if creature lives in air, yellow
-        
-        */
-        alert("showHome()");
-    
-}//end showHome()
+function showHome() {
+    var creatures = document.querySelectorAll(".creature");
 
-function showType()
-{
-    /*
-        loop through all creatures
-        
-        if creature is an animal, border color solid red
-        if creature is an insect, border color is dashed orange
-    */
-    alert("showType()");
-}//end showType()
+    for (var i = 0; i < creatures.length; i++) {
+        if (creatures[i].dataset.home == "land") {
+            creatures[i].style.backgroundColor = "green";
+        } else if (creatures[i].dataset.home == "water") {
+            creatures[i].style.backgroundColor = "blue";
+        } else if (creatures[i].dataset.home == "air") {
+            creatures[i].style.backgroundColor = "yellow";
+        }
+    }
+}
 
-function clearAll()
-{
-    /*
-        loop through all creatures
-        
-        clear border back to 1px solid black
-        clear background color to white
-        
-    */
-    alert("clearAll()");
-}//end clearAll()
+function showType() {
+    var creatures = document.querySelectorAll(".creature");
+
+    for (var i = 0; i < creatures.length; i++) {
+        if (creatures[i].dataset.type == "animal") {
+            creatures[i].style.border = "2px solid red";
+        } else if (creatures[i].dataset.type == "insect") {
+            creatures[i].style.border = "2px dashed orange";
+        }
+    }
+}
+
+function clearAll() {
+    var creatures = document.querySelectorAll(".creature");
+
+    for (var i = 0; i < creatures.length; i++) {
+        creatures[i].style.backgroundColor = "white";
+        creatures[i].style.border = "2px solid black";
+    }
+}
